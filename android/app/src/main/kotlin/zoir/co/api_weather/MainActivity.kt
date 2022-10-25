0b31048e8dc2b1f081a44548529f143b8cea9e7d
Zoir I <wowdobryy@gmail.com>
2022-10-25 17:59:55 +0600

    weather app with free api from openweather.org

---
+++ android/app/src/main/kotlin/zoir/co/api_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package zoir.co.api_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WeatherApp(),
+    );
+  }
+}
+
+
+class WeatherApp extends StatefulWidget {
+  const WeatherApp({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherApp> createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
